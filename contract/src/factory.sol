333122cafc58ee0a8ff117ad202083cc8d37ac73
Maa-ly <lydiaahenkorahgyamfi@gmail.com>
2026-01-21 16:04:30 +0000

    deployment

--- contract/src/factory.sol
+++ contract/src/factory.sol
@@ -5,39 +5,27 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract FactoryMech {
     address public immutable intentImplementation;
-    address public immutable escrowImplementation;
 
     mapping(address => address[]) public userIntents;
 
     event IntentCloneCreated(address indexed user, address intentClone);
 
-    constructor(address _intentImpl, address _escrowImpl) {
+    constructor(address _intentImpl) {
         intentImplementation = _intentImpl;
-        escrowImplementation = _escrowImpl;
     }
 
     function createIntentClone() external returns (address intentClone) {
         intentClone = Clones.clone(intentImplementation);
 
         // initialize clone
-        (bool ok,) = intentClone.call(
-            abi.encodeWithSignature(
-                "initialize(address,address)",
-                msg.sender,
-                escrowImplementation
-            )
-        );
+        (bool ok,) = intentClone.call(abi.encodeWithSignature("initialize(address)", msg.sender));
         require(ok, "Init failed");
 
         userIntents[msg.sender].push(intentClone);
         emit IntentCloneCreated(msg.sender, intentClone);
     }
 
-    function getUserIntents(address user)
-        external
-        view
-        returns (address[] memory)
-    {
+    function getUserIntents(address user) external view returns (address[] memory) {
         return userIntents[user];
     }
 }
